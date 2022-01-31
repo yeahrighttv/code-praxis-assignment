49912a7c85b77689f6d7767ce8b83688adbd2bf4
yeahrighttv <51854314+yeahrighttv@users.noreply.github.com>
2022-01-31 16:55:28 +0100

    just let me upload

---
+++ test.py
@@ -0,0 +1,6 @@
+'''System module'''
+import unittest
+
+if __name__ == '__main__':
+    testsuite = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity = 2).run(testsuite)
